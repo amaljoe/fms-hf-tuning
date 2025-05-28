1b437610285ecef82f0ed4bcde221e6b90798d7f
amal <amaljoe@ibm.com>
2025-05-28 15:41:12 +0530

    change take training signal order

--- tuning/utils/online_data_loader.py
+++ tuning/utils/online_data_loader.py
@@ -136,13 +136,14 @@ class  OnlineDataLoader(IterableDataset):
                 continue
             batch['metadata'] = {'domain_index': index}
             loss = 5 if index == 0 else 0
-            self.take_training_signals(batch, loss)
 
             # tokenize
             if self.tokenizer:
                 if 'input' in batch:
                     batch['input_ids'] = self.tokenizer(batch['input'], padding=True, truncation=True, return_tensors='pt')['input_ids']
                     batch['attention_mask'] = self.tokenizer(batch['input'], padding=True, truncation=True, return_tensors='pt')['attention_mask']
+
+            self.take_training_signals(batch, loss)
             yield batch
 
     def __len__(self):
